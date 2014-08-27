4a812ad4843f29fcb39142af56d523e26b9ab67a
Pavel Verkhovtsov <pavel_verkhovtsov@epam.com>
2014-08-27 14:10:31 +0300

    Add CheckStyle

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -1,46 +1,68 @@
 package Matrix;
 
 public class Matrix {
-	private int power = 3;
+	private final double eps = 1e-21;
+	private int matrixExtent = 3;
 	private double[][] systemCoefficients  = {{2.18, 2.44, 2.49},
 											  {2.17, 2.31, 2.49},
 											  {3.15, 3.22,3.17 },
 											  {-4.34, -3.91, -5.27}};
-	
-	
+
+	private boolean systemDoesNotHaveSolutions = false;
 	//	private static double freeCoefficients[] = { -4.34, -3.91, -5.27};
-	
-	
-	
-	
-	public Matrix(int p, double[][] a, double[] b){
-		this.power = p;
-		this.systemCoefficients = a;
-		this.systemCoefficients[power]= b;
+
+//TODO: Check system solvability.
+
+	/**
+	 * Constructor.
+	 * @param size Matrix size
+	 * @param sysCoeff Array of system coefficients
+	 * @param freeCoeff Array of free coefficients
+	 */
+	public Matrix(final int size, final double[][] sysCoeff, final double[] freeCoeff){
+		this.matrixExtent = size;
+		this.systemCoefficients = sysCoeff;
+		this.systemCoefficients[matrixExtent]= freeCoeff;
 	}
-	
-	public void swapRow(int rowNumber){
+
+	/**
+	 * Change element in the row with maximum element.
+	 * @param rowNumber Number of row
+	 */
+	public void swapRow(final int rowNumber){
 		double buffer;
-		int maxIndex = getIndexOfMaxElement(rowNumber); 
-		for(int j=rowNumber; j<=power; j++){
+		int maxIndex = getIndexOfMaxElement(rowNumber);
+		for(int j=rowNumber; j<=matrixExtent; j++){
 			buffer=systemCoefficients[rowNumber][j];
 			systemCoefficients[rowNumber][j] = systemCoefficients[maxIndex][j];
 			systemCoefficients[maxIndex][j]=buffer;
-		}	
+		}
 	}
-		
-	private int getIndexOfMaxElement(int rowNumber){
+
+	/**
+	 * Search maximum modulus element in row.
+	 * @param rowNumber Number of row
+	 * @return index maximum modulus element
+	 */
+	private int getIndexOfMaxElement(final int rowNumber){
 	    double max = Math.abs(systemCoefficients[rowNumber][rowNumber]);
 		int maxIndex = rowNumber;
-		for (int i=rowNumber+1; i<power; i++){
+		for (int i=rowNumber+1; i<matrixExtent; i++){
 			if(Math.abs(systemCoefficients[i][rowNumber]) > max){
 				max = Math.abs(systemCoefficients[i][rowNumber]);
 				maxIndex = i;
 			}
 		}
-		
-		//
-		return maxIndex;	
+
+		if(max < eps){
+			systemDoesNotHaveSolutions = true;
+		}
+		return maxIndex;
 	}
+	
+	public void divideRow(final int rowNumber){
+		
+	} 
+	
 	
 }
